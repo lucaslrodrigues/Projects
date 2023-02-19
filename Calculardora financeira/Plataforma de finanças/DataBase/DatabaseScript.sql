05952eef54c8b8d419644aa9220ee4af21f3bc1a
Lucas <lucas.lrodrigues@sptech.school>
2023-02-18 23:28:58 -0300

    tela de login responsivel e inicio do banco de dados

---
+++ Calculardora financeira/Plataforma de finanças/DataBase/DatabaseScript.sql
@@ -0,0 +1,10 @@
+create database profitoVenerdi;
+use profitoVenerdi;
+
+create table user (
+id int primary key auto_increment,
+name varchar(45),
+email varchar(45),
+senha varchar(45),
+NascDate date
+);
